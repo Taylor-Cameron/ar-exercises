1c07e91dc1e37cc7c35c84e195b860748817c13b
Taylor Cameron <taylorcameron@Taylors-MacBook-Air.local>
2018-12-04 16:01:58 -0700

    exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+print 'total revenue: ', Store.sum(:annual_revenue)
+puts
+
+print 'average store revenue: ', (Store.sum(:annual_revenue) / Store.count)
+puts
+
+print 'store where the revenue is over 1000000: ', Store.where("annual_revenue > ? ", 1000000).count
+puts
