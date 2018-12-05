6e9bd837f4299be0f2f98f629a8c870d68333601
Taylor Cameron <taylorcameron@Taylors-MacBook-Air.local>
2018-12-05 12:45:20 -0700

    exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,8 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "what is the stores name?"
+
+store_name = gets.chomp
+Store.create(name: store_name, annual_revenue: 0, mens_apparel: false, womens_apparel: false)
