36f2678436d2cae6d1f5df936892c0583aaf6bd3
Taylor Cameron <taylorcameron@Taylors-MacBook-Air.local>
2018-12-05 12:23:45 -0700

    exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+realStore1 = @store1[0]
+realStore1.employees.create(first_name: "Taylor", last_name: "Cameron", hourly_rate: 120)
+
+# employee1 = Employee.create(first_name: "Taylor", last_name: "Cameron", hourly_rate: 120)
+
+# employee1.store = @store1.id
+
+# employee1.save
+
+# p employee1
